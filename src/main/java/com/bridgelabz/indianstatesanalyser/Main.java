47ce751df045b8fc182edaf20bc8fe17db1467d2
unknown <mittalshubham05@gmail.com>
2020-10-18 16:53:51 +0530

    Add: added functionality for throwing custom exceptions when incorrect data

--- src/main/java/com/bridgelabz/indianstatesanalyser/Main.java
+++ src/main/java/com/bridgelabz/indianstatesanalyser/Main.java
@@ -1,12 +1,14 @@
 package com.bridgelabz.indianstatesanalyser;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class Main 
 {
-    public static void main ( String[] args ) throws CustomException{
+    public static void main ( String[] args ) throws CustomException, IOException{
         System.out.println( "Hello World!" );
         StateCensusAnalyser.readStatesCensusFromCsv("IndiaStateCensusData - IndiaStateCensusData.csv");
     }
