eb58b8e53d2fa3d75ad4838ef8db1f581eaa662b
unknown <mittalshubham05@gmail.com>
2020-10-18 19:10:59 +0530

    Add: added functionality for checking type is given to csvToBeanBuilder

--- src/main/java/com/bridgelabz/indianstatesanalyser/CSVStates.java
+++ src/main/java/com/bridgelabz/indianstatesanalyser/CSVStates.java
@@ -16,27 +16,28 @@ import com.opencsv.bean.CsvToBeanBuilder;
  * @author Shubham, parses the states code csv file for operations
  */
 public class CSVStates {
-	public static int readStatesCensusFromCsv(String fileName) throws IOException{
+	public static int readStatesCensusFromCsv(String fileName) throws IOException {
 		Logger logger = LogManager.getLogger(CSVStates.class);
-		int count = 1 , val;
+		int count = 1, val = 0;
 		Reader reader = null;
-		reader = Files.newBufferedReader(Paths.get(fileName));
-		CsvToBean<StateCodes> csvToBean = new CsvToBeanBuilder<StateCodes>(reader).withType(StateCodes.class)
-				.withIgnoreLeadingWhiteSpace(true).withThrowExceptions(false).build();
-		Iterator<StateCodes> iterator = csvToBean.iterator();
-		while (iterator.hasNext()) {
-			StateCodes states = iterator.next();
-			logger.info(states + " got from " + (count++) + " object");
-		}
-		val = count - 1;
-		csvToBean.getCapturedExceptions().stream().forEach(t-> {
-			try{
-				throw new CustomException(t.toString());
-			}catch(CustomException exception) {
-				logger.error(exception.getMessage());
+		try {
+			count = 1;
+			reader = Files.newBufferedReader(Paths.get(fileName));
+			CsvToBean<StateCodes> csvToBean = new CsvToBeanBuilder<StateCodes>(reader).withIgnoreLeadingWhiteSpace(true)
+					.build();
+			Iterator<StateCodes> iterator = csvToBean.iterator();
+			while (iterator.hasNext()) {
+				StateCodes states = iterator.next();
+				logger.info(states + " got from " + (count++) + " object");
+				val = count - 1;
 			}
-			});
-
-		return val;
+		} catch (IllegalStateException exception) {
+			logger.error(exception.getMessage());
+			throw new CustomException(exception.getMessage());
+		} catch (Exception exception) {
+			System.out.println("error general");
+		} finally {
+			return val;
+		}
 	}
 }
