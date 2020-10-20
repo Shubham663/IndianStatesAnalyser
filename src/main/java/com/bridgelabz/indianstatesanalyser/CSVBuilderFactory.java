62dd8ec694a0d68848e81d0306237f6f2a38a243
unknown <mittalshubham05@gmail.com>
2020-10-20 12:36:03 +0530

    Edit: created jar for services and used inside Census Analyser

--- src/main/java/com/bridgelabz/indianstatesanalyser/CSVBuilderFactory.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.indianstatesanalyser;
-
-public class CSVBuilderFactory {
-	public static ICSVBuilder generateBuilder() {
-		return new OpenCSVBuilder();
-	}
-}
--- src/main/java/com/bridgelabz/indianstatesanalyser/ICSVBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.indianstatesanalyser;
-
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Iterator;
-
-public interface ICSVBuilder {
-	public<T> Iterator getIterator(String fileName, Reader reader,T whichClass) throws IOException, CustomException;
-}
